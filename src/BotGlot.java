9c1531d0a26fb7f26395b2d67f3c30d553329af8
jquad <jordanaquadro@gmail.com>
2019-09-23 17:37:37 -0400

    add code from PFJ16's bot to be reviewed and modified

--- src/BotGlot.java
+++ src/BotGlot.java
@@ -1,9 +1,38 @@
+import java.util.logging.Level; //is this needed for mine?
+import java.util.logging.Logger; //is this needed for mine?
+
+/**
+ *
+ * @author Ursula Jordan Aquadro
+ */
+
+
+
 public class BotGlot {
 
+    /**
+     * @param args creates the actions object
+     */
 
     //Create main method to run actions w/ action object
     public static void main(String[] args) {
-        
+        TwitterActions actions = new TwitterActions ();
+
+        /*
+        is any of this needed?
+        //call your function here. example:
+
+        int i=1;
+        while (i>0){
+            actions.searchTweetandRetweet("#austria");
+            try {
+                Thread.sleep (300000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(TwitterBot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+*/
     }
 
 
--- src/BotGlotAction.java
+++ src/BotGlotAction.java
@@ -1,7 +1,34 @@
-public class BotGlotAction {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import twitter4j.IDs;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
-    //import twitter4j packages
-    //import java packages
+/**
+ *
+ * @author Ursula Jordan Aquadro
+ */
+
+public class BotGlotAction {
+    //Create bot name
+    public static final String USER_NAME_BOT = "BotGlot";
+    Twitter twitter;
+    long lastSTatusTime;
+    ArrayList<Status> tweetAlreadyused; //need this?
+    ArrayList<Status> tweets;
+
 
 
     //Need to search for desired users
@@ -9,4 +36,78 @@ public class BotGlotAction {
     //Need to to follow desired users
 
     //Need to retweet content of desired users into bot's TL
+    /*
+    from PJF16's twitterbot code - need to review and change
+
+
+        //Works but tries to retweet some Tweets twice. Checking...
+
+    public void searchTweetandRetweet(String hashtag) {
+        Query query = new Query(hashtag);
+        query.count(40);
+        int check = 0;
+        int alreadyused = 0;
+        int remove = 0;
+
+        try {
+            QueryResult queryresult = this.twitter.search(query);
+            this.tweets = (ArrayList<Status>) queryresult.getTweets();
+            Collections.reverse(tweets);
+            for (Status status : tweets) {
+                if (!status.isRetweetedByMe() && status.getCreatedAt().getTime() > this.lastStatusTime) {
+                    try {
+                        this.retweetTweet(status);
+                        this.lastStatusTime = status.getCreatedAt().getTime();
+                        this.writeTimeToFile();
+                        Thread.sleep(10000);
+                    } catch (TwitterException tex) {
+                        System.out.println("Twitter Exception - Already Retweeted");
+                    }
+                }
+            }
+        } catch (TwitterException ex) {
+            Logger.getLogger(TwitterActions.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (InterruptedException ex) {
+            Logger.getLogger(TwitterActions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void retweetTweet(Status status) throws TwitterException {
+        this.twitter.retweetStatus(status.getId());
+    }
+
+    private long lastRetweetFromFile() {
+        long lastretweet = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("time.dat"));
+            lastretweet = Long.parseLong(br.readLine());
+        } catch (FileNotFoundException ex) {
+            lastretweet = new Date().getTime();
+            try {
+                PrintWriter pw = new PrintWriter("time.dat");
+                pw.print(new Date().getTime());
+                pw.close();
+            } catch (FileNotFoundException ex1) {
+                Logger.getLogger(TwitterActions.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        } catch (IOException ex) {
+            lastretweet = new Date().getTime();
+            try {
+                PrintWriter pw = new PrintWriter("time.dat");
+                pw.print(new Date().getTime());
+                pw.close();
+            } catch (FileNotFoundException ex1) {
+                Logger.getLogger(TwitterActions.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        }
+
+        return lastretweet;
+
+    }
+
+
+
+
+     */
 }
